d44fc9bc4aca2f40d165895bb652c145b1ce6119
idrissohs <idriss@live.ca>
2015-10-28 22:34:18 -0400

    changes to index.html and main.js

--- public/js/forms.js
+++ public/js/forms.js
@@ -141,16 +141,6 @@
 	// Dummy Submit Form (Remove this)
 	//----------------------------------------------
 	// This is just a dummy form submission. You should use your AJAX function or remove this function if you are not using AJAX.
-  function dummy_submit_form($form)
-  {
-  	if($form.valid())
-  	{
-  		form_loading($form);
-  		
-  		setTimeout(function() {
-  			form_success($form);
-  		}, 2000);
-  	}
-  }
+
 	
 })(jQuery);
