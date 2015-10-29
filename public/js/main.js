7519de20a814be39e2974dbba88cda392b4db6b3
idrissohs <idriss@live.ca>
2015-10-29 01:40:40 -0400

    changes to authentication functions

--- public/js/main.js
+++ public/js/main.js
@@ -2,32 +2,23 @@ $(document).ready(function(){
 	
 	
 	// Sign up form code starts
-	$("#signUpForm").submit(function(e){
-		e.preventDefault();
-		signUpUser();
-	})
 	
-	$("#main").click(function(e) {
-		$("#main").load("html/SignupForm.html");
+	$("#SignUp").click(function(e) {
+		$("#signupform").load("html/SignupForm.html");
 	});
 	
-	function signUpUser(){
-		var user = new Parse.User;
-		
-		user.set("username", $("#signUpForm #userName").val());
-		user.set("password", $("#signUpForm #password").val());
-		
-		user.signUp(null, {
-		  success: function(user) {
-			alert("You have signed up!");
-		  },
-		  error: function(user, error) {
-			// Show the error message somewhere and let the user try again.
-			alert("Error: " + error.code + " " + error.message);
-			$('#signUpForm').trigger("reset");
-		  }
-		});
+
+	function checklogin(){
+		if (Parse.User.current()) {
+			$("#currentUser").html("Logged in as"+Parse.User.current().get("username"));
+			$("#SignUp").hide();
+		}
+		else{
+			$("#currentUser").load("html/LoginForm.html");
+		}
 	}
+	checklogin();
+	
 	
 
 })
