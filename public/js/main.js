74c7b8cc76bc70b3567e0da0b58e2934fb521774
idrissohs <idriss@live.ca>
2015-10-28 21:15:20 -0400

    updated files

---
+++ public/js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	
+	
+	// Sign up form code starts
+	$("#signUpForm").submit(function(e){
+		e.preventDefault();
+		
+		signUpUser();
+	})
+	
+	function signUpUser(){
+		var user = new Parse.User;
+		
+		user.set("username", $("#signUpForm #userName").val());
+		user.set("password", $("#signUpForm #password").val());
+		
+		user.signUp(null, {
+		  success: function(user) {
+			alert("You have signed up!");
+		  },
+		  error: function(user, error) {
+			// Show the error message somewhere and let the user try again.
+			alert("Error: " + error.code + " " + error.message);
+			$('#signUpForm').trigger("reset");
+		  }
+		});
+	}
+	
+	function displayProfile(user){
+		document.getElementById("main").innerHTML=""
+	}
+	
+	// Sign up form code ends
+	
+})
